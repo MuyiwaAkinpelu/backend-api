83f2d8ef4a30965d4935fd00366ff8090fa4f264
Muhammad Alhajee <alhajee2009@gmail.com>
2024-08-25 12:19:24 +0100

    refactor: password reset link

--- src/modules/auth/dto/request-reset-password.dto.ts
+++ src/modules/auth/dto/request-reset-password.dto.ts
@@ -2,7 +2,7 @@ import { IsEmail, IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class RequestResetPasswordDto {
-  @ApiProperty({ type: String })
+  @ApiProperty({ type: String, example: 'alhajee2009@gmail.com' })
   @IsEmail()
   @IsNotEmpty()
   readonly email!: string;
